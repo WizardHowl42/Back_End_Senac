53798c10c8a66d8dd45a4082f5633a01871fe62c
Jonatas <jonatas52807956@edu.pe.senac.br>
2023-12-11 19:58:51 -0300

    Criação do arquivo index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Adicionando os arquivos no git
+// git add . | git add --all | git add -A | git add nome_do_arquivo.js
+// git restore nome_do_arquivo.js -> 
+// configuração de usuário : got config --local/global user.name "Jonatas" user.email ""
+console.log("boa noute");
